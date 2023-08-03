f4935ed4a4228387785503490d4317d3e9df4d7b
Иван <ivanhlop@gmail.com>
2023-08-03 11:14:30 +0300

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
     }
+
+    public static void reverseArray(int[] array) {
+        int[] array2 = new int [array.length];
+        for (int i = 0; i < array.length; i++) {
+            array2[i] = array[array.length - 1 - i];
+        }
+        System.out.println(Arrays.toString(array2));
+    }
+
+    public static void task1() {
+        System.out.println("Задание 1");
+        int[] array = {1, 4, 5, 6, 7, 8};
+        reverseArray(array);
+    }
+
 }
